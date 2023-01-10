dc977ed7b770aa53b66b4ca162178dd5053c36c3
Emma Cheng <ecolipp@gmail.com>
2023-01-10 14:32:01 +0800

    Add note.

--- src/components/Demo1/index.tsx
+++ src/components/Demo1/index.tsx
@@ -29,12 +29,12 @@ type dataType = {
   text: string;
 };
 
-function Demo1 () {
+function Demo1() {
   const [list, setList] = useState<dataType[] | []>([]);
 
-  const onDragEnd = (result:any) => {
+  const onDragEnd = (result: any) => {
     const newItems = Array.from(list);
-    const [removed] = newItems.splice(result.source.index, 1);
+    const [removed] = newItems.splice(result.source.index, 1); //拖曳哪一個index
     newItems.splice(result.destination.index, 0, removed);
     console.log(newItems); // 拖曳結果
     setList(newItems);
@@ -43,22 +43,28 @@ function Demo1 () {
   const renderItems = () => {
     const output = list.map((t, i) => (
       <Draggable draggableId={t.id} index={i} key={t.id}>
-        {(p:any) => (
+        {(p: any) => (
           <Item
             ref={p.innerRef}
             {...p.draggableProps}
             {...p.dragHandleProps}
             key={t.id}
           >
-            <Radio type='checkbox' />
+            <Radio type="checkbox" />
             {t.text}
-            <Btn type='button' value='查詢' onClick={() => {console.log('點擊id:', t.id)}} />
+            <Btn
+              type="button"
+              value="查詢"
+              onClick={() => {
+                console.log("點擊id:", t.id);
+              }}
+            />
           </Item>
         )}
       </Draggable>
     ));
     return output;
-  }
+  };
 
   useEffect(() => {
     setList(data);
@@ -66,11 +72,9 @@ function Demo1 () {
 
   //如果要vertical就移掉direction="horizontal"
   return (
-    <DragDropContext
-      onDragEnd={onDragEnd}
-    >
+    <DragDropContext onDragEnd={onDragEnd}>
       <Droppable droppableId="droppable-1" direction="horizontal">
-        {(provided:any) => (
+        {(provided: any) => (
           <Container ref={provided.innerRef} {...provided.droppableProps}>
             {list && renderItems()}
             {provided.placeholder}
